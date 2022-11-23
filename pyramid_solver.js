c25e085d539fe3924bdcd214c749396f1f8d81f8
Umahr <bradvogt1@gmail.com>
2022-11-22 21:40:11 -0500

    Add files via upload

---
+++ pyramid_solver.js
@@ -0,0 +1,86 @@
+/**
+ * Solve the pyramid puzzle 
+ */
+
+function readFile(fileList){
+	let file = fileList[0];
+	
+ 	let reader = new FileReader(); 	
+ 	reader.addEventListener('load', function(){ process(reader.result)});
+ 	reader.readAsText(file);
+ 	
+}
+
+function process(input){
+	var vals = input.split(/\r?\n/);
+	var target = vals[0].replace('Target: ', '');
+	var total = "0";
+	var depth = "0";
+	var position = "0";
+	var goRight = 0;
+	var maxDepth;
+	var directions = new Array();
+	
+	vals.shift();
+	maxDepth  = vals.length - 1;
+	for(let i = 0; i <= maxDepth; ++i){
+		vals[i] = vals[i].split(',');
+	}
+	
+	total = vals[0];
+	/* 
+	 * we are always going as far down the left branch as possible
+	 * if we reach a point where we cannot continue we will go back up a level, continue down
+	 * the other node if not already traversed, and continue prioritising going left.
+	*/
+	while(total != target || (total == target && depth != maxDepth)){
+		//navigate back up the tree if you reach the end or become too large
+		if(total >= target || depth == maxDepth){
+			if(directions[depth-1] == "L"){
+				total = total / vals[depth][position];
+				depth--;
+				directions.pop();
+				goRight = 1;
+			} else{
+				//go up the entire right chain
+				while(directions[depth-1] == "R"){
+					total = total / vals[depth][position];
+					depth--;
+					position--;
+					directions.pop();
+				}
+				//now go up from the completely checked left node. if there is no where else to go, exit
+				if(depth == 0){
+					let error = "The value was not found in the pyramid.";
+					document.getElementById("output").innerHTML = error;
+					break;
+				}else{
+					total = total / vals[depth][position];
+					depth--;
+					directions.pop();				
+					goRight = 1;
+				}
+			}
+		}
+		//go right
+		if (goRight == 1){
+			directions.push("R");
+			depth++;
+			position++;
+			goRight = 0;
+			total = total * vals[depth][position];
+		}else {  //go left
+			directions.push("L");
+			depth++;
+			total = total * vals[depth][position];
+		}
+	
+	
+
+	}
+		document.getElementById("output").innerHTML = directions.join("");
+} 
+ 
+ const fileSelect = document.getElementById('file_upload');
+ fileSelect.addEventListener('change', function(){ readFile(event.target.files) });
+ 
